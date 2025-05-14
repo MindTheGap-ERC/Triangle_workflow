8373cb6bf30d2a9a1ce888f37f5c40d6178dae26
Emilia Jarochowska <e.b.jarochowska@uu.nl>
2025-05-14 15:11:41 +0200

    initial R script, still not good

---
+++ src/R/automatic_stratigraphy.R
@@ -0,0 +1,38 @@
+#!/usr/bin/env Rscript
+# Running R script with parameters from CLI:
+# See https://www.r-bloggers.com/2015/09/passing-arguments-to-an-r-script-from-command-lines/
+
+#### Imports ####
+library("astrochron")
+
+args = commandArgs(trailingOnly=TRUE)
+
+df <- read.csv(file=args[1],
+               sep = ",",
+               header = TRUE)
+
+
+strat_columns <- split_sections(df)
+
+#### Test this on one section: needs to be propagated to an entire list ####
+
+strat_columns$sc1_collapsed <- collapse_section(strat_columns$sc1)
+da <- strat_columns$sc1_collapsed
+da$thickness <- da$thickness * 100 #convert m to cm
+
+duration = 400 # kyr, needs to be read from the metadata
+
+av_sed = da$thickness[1]/duration #cm/kyr
+prec_period = 21*av_sed #cm
+N_per_cycle = 6 
+dh = prec_period/N_per_cycle
+
+data = astrochron::linterp(da, dt = dh/100,
+                           check = TRUE)
+res_m = 0.1
+
+ranked_data = astrochron::rankSeries(data, dt = res_m)
+
+a = astrochron::timeOpt(ranked_data, output = 1)
+
+
